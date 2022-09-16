458fdecaa056fb785f6f55778519a0f0d310b1f2
Sa-zuluaga <szuluagas@devco.com.co>
2022-09-16 07:04:50 -0500

    se agrega clase de puebras unitarias

---
+++ src/test/java/com/sazuluaga/semillero/counter/WordCounterDefaultTest.java
@@ -0,0 +1,11 @@
+package com.sazuluaga.semillero.counter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WordCounterDefaultTest {
+    @Test
+    void mustReturnCorrectSummary() {
+        Assertions.fail();
+    }
+}
